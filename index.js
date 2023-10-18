fd842d72a5bc04c57a3949b03a5dcbf00b99f111
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-10-19 01:59:53 +0600

    get method added to get brand collection

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const brandsCollection = await client.db("brandsDB").collection("brands");
     const productCollection = await client
@@ -34,6 +34,11 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/brands", async (req, res) => {
+      const cursor = brandsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // send to server from form input
     app.post("/products", async (req, res) => {
@@ -41,6 +46,14 @@ async function run() {
       console.log(newProduct);
 
       const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // send to server from form input
+    app.post("/brands", async (req, res) => {
+      const brand = req.body;
+      console.log(brand);
+
+      const result = await brandsCollection.insertOne(brand);
       res.send(result);
     });
 
